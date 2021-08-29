3e5f07072fc435acac25b96f822afb71292bd292
Noah Lidell <noahlidell@gmail.com>
2021-08-29 16:35:26 -0500

    set codex runtime dir in lua conf

--- lua/codex.lua
+++ lua/codex.lua
@@ -2,6 +2,9 @@ local M = {}
 
 local plugin_dir = vim.fn.fnamemodify(vim.api.nvim_get_runtime_file("lua/codex.lua", false)[1], ":h:h")
 vim.fn.setenv("CODEX_HOME", plugin_dir)
+-- set codex runtime dir here using some user config setting
+local codex_runtime_dir = vim.loop.os_homedir() .. ".local/share/codex"
+vim.fn.setenv("CODEX_RUNTIME_DIR", codex_runtime_dir)
 
 local binary_path = plugin_dir .. "/target/debug/codex"
 if vim.fn.executable(binary_path) == 0 then
